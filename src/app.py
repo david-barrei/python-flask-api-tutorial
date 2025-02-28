a53f00d3ea735418ae418478231ce00cf3866f23
David Barreiro <86165842+david-barrei@users.noreply.github.com>
2025-02-28 05:46:44 +0000

    terminado post

--- src/app.py
+++ src/app.py
@@ -10,19 +10,17 @@ todos = [
 ]
 
 
+@app.route('/todos', methods=['GET'])
+def hello_world():
+    return jsonify(todos)
+
 @app.route('/todos', methods=['POST'])
 def add_new_todo():
     request_body = request.json
     print("Incoming request with the following body", request_body)
-    request_body = jsonify(todos)
-    return request_body
+    todos.append(request_body)
+    return jsonify(request_body)
 
-
-
-@app.route('/todos-read', methods=['GET'])
-def hello_world1():
-    json_text = jsonify(todos)
-    return json_text
 
 
 if __name__ == '__main__':
