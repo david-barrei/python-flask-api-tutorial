8bb17cf52a2d74f9a9462e8a4f63c4a7089700e4
David Barreiro <86165842+david-barrei@users.noreply.github.com>
2025-02-28 04:51:08 +0000

    agregando POST

--- src/app.py
+++ src/app.py
@@ -1,11 +1,38 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask import request
+
+
 app = Flask(__name__)
 
-@app.route('/todos', methods=['GET'])
-def hello_world():
-    return '<h1>Hello!</h1>'
+todos = [
+    { "label": "My first task", "done": False },
+    { "label": "My second task", "done": False }
+]
 
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.json
+    print("Incoming request with the following body", request_body)
+    request_body = jsonify(todos)
+    return request_body
+
+@app.route('/todos/prueba', methods=['POST'])
+def add_new_todo():
+    request_body = request.json
+    print("Incoming request with the following body", request_body)
+    request_body = jsonify(todos)
+    return request_body
+
+
+
+
+@app.route('/todos-read', methods=['GET'])
+def hello_world1():
+    json_text = jsonify(todos)
+    return json_text
 
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
+
